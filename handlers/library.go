b4a6ec5a9b13fff7cbccccbf9a0fd5b790d25a25
chandudagoat <chandulanka1706@gmail.com>
2023-12-15 01:28:01 +0530

    create library complete

--- handlers/library.go
+++ handlers/library.go
@@ -1,7 +1,32 @@
 package handlers
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"context"
+	"fmt"
+	"goapi/database"
+
+	"github.com/gofiber/fiber/v2"
+)
+
+type libraryDTO struct {
+	Name    string `json: "name" bson: "name"`
+	Address string `json: "address" bson: "address"`
+}
 
 func CreateLibrary(ctx *fiber.Ctx) error {
-	return "herl"
+	new_lib := new(libraryDTO)
+
+	if err := ctx.BodyParser(new_lib); err != nil {
+		return err
+	}
+
+	fmt.Println(new_lib)
+	libCollection := database.GetCollection("libraries")
+	new_doc, err := libCollection.InsertOne(context.TODO(), new_lib)
+
+	if err != nil {
+		return err
+	}
+
+	return ctx.JSON(fiber.Map{"id": new_doc.InsertedID})
 }
--- router.go
+++ router.go
@@ -6,19 +6,16 @@ import (
 	"github.com/gofiber/fiber/v2"
 )
 
-func routes(app *fiber.App) error {
+func routes(app *fiber.App) {
 	// create a health check route
 	app.Get("/health", func(ctx *fiber.Ctx) error {
 		return ctx.SendString("Ok")
 	})
-
-	return nil
 }
 
-func groups(app *fiber.App) error {
+func groups(app *fiber.App) {
 	// create the library routes
 	libGroup := app.Group("/library")
 	libGroup.Get("/test", handlers.TestHandler)
-
-	return nil
+	libGroup.Post("/create", handlers.CreateLibrary)
 }
